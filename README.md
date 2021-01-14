de34cf979ad5c737152dbaa737262886bd5bf2c9
Jens Prangenberg <jens.prangenberg@check24.de>
2021-01-14 18:57:56 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# moneymoney-tresorone
-Tresor.one extension for MoneyMoney
+# MoneyMoney Tresor.one
+
+Fetches portfolio from https://tresor.one
+
+## Setup
+
+* Download Tresor.one extension (TresorOne.lua)
+* In MoneyMoney app open “Help” Menu and hit “Show database in finder” (https://moneymoney-app.com/extensions/#installation)
+* Copy TresorOne.lua in extensions folder
+* In MoneyMoney app open “Preferences” > “Extensions” and make sure TresorOne shows up (to use unsigned extension uncheck “verify digital signatures of extensions” at the bottom)
+
+### Add Account
+
+* Use your portfolio hash as the username (https://app.tresor.one/p/**5f55425d139fc90007978e75**)
+* You need no password (you can use "123", save the password)
+
+## Known Issues and Limitations
+
+* Your portfolio must be public until Tresor.one will release an API for third party applications
---
+++ TresorOne.lua
@@ -0,0 +1,54 @@
+WebBanking{
+  version = 1.0,
+  url = 'https://tresor.one',
+  services = {'Tresor.one'},
+  description = 'Get portfolio for Tresor.one'
+}
+
+function SupportsBank (protocol, bankCode)
+  return bankCode == 'Tresor.one' and protocol == ProtocolWebBanking
+end
+
+local connection
+local portfolio
+
+function InitializeSession (protocol, bankCode, username, username2, password, username3)
+  connection = Connection()
+  portfolio = username
+end
+
+function ListAccounts (knownAccounts)
+  local account = {
+    name = 'Tresor.one',
+    accountNumber = 'Portfolio',
+    portfolio = true,
+    type = AccountTypePortfolio
+  }
+
+  return {account}
+end
+
+function RefreshAccount (account, since)
+  local securities = {}
+
+  response = connection:get('https://api.tresor.one/v1/portfolios/' .. portfolio)
+  json = JSON(response):dictionary()
+
+  local holdings = json['holdings']
+  for index, values in pairs(holdings) do
+    securities[#securities+1] = {
+      name = values['security']['name'],
+      isin = values['security']['isin'],
+      securityNumber = values['security']['wkn'],
+      currency = nil,
+      quantity = values['position']['shares'],
+      price = values['position']['currentPrice'],
+      purchasePrice = values['position']['purchasePrice']
+    }
+  end
+
+  return {securities=securities}
+end
+
+function EndSession ()
+end
